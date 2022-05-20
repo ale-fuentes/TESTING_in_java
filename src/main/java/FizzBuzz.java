43b404fa6a5d1214846bcdab286b779aeff4e8d8
alefuuu-linux <fuentesra@hotmail.com>
2022-05-20 07:45:38 -0300

    feat - ajust fizzbuzz class

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -4,8 +4,10 @@ import java.util.List;
 public class FizzBuzz {
     public List<String> getNumbers() {
         List<String> numbers = new ArrayList<String>();
-        numbers.stream().fo
-        for(int i=0; i<100 ; i++)
+
+        for(int i=0; i<100 ; i++){
+            numbers.add(String.valueOf(i));
+        }
         return numbers;
     }
 }
